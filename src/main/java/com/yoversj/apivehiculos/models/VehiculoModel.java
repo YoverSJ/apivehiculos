2b2ae37930860f6911c739503619a11d97e165ab
YoverSJ <ysantiago@geosatelital.com.pe>
2023-05-25 20:24:57 -0500

    implementacion de la clase Modelo

--- src/main/java/com/yoversj/apivehiculos/models/VehiculoModel.java
+++ src/main/java/com/yoversj/apivehiculos/models/VehiculoModel.java
@@ -1,5 +1,39 @@
 package com.yoversj.apivehiculos.models;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "vehiculos")
 public class VehiculoModel {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    private String brand;
+
+    private String model;
+
+    private String plate;
+
+    private int year;
+
+    private String type;
+
+    private LocalDateTime created;
+
+    private LocalDateTime modified;
+
+    private int status;
+
 }
