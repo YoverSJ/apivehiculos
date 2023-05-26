bee2c5c52468d9b2427accb8a08baf39d320a213
YoverSJ <ysantiago@geosatelital.com.pe>
2023-05-25 21:20:55 -0500

    Se agrego la respuesta Http a los metodos y se agrego valor al campo created

--- src/main/java/com/yoversj/apivehiculos/controllers/VehiculoController.java
+++ src/main/java/com/yoversj/apivehiculos/controllers/VehiculoController.java
@@ -1,5 +1,6 @@
 package com.yoversj.apivehiculos.controllers;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yoversj.apivehiculos.models.VehiculoModel;
@@ -20,12 +23,16 @@ public class VehiculoController {
     private VehiculoService vehiculoService;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public ArrayList<VehiculoModel> obtenerVehiculos() {
         return vehiculoService.obtenerVehiculos();
     }
     
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public VehiculoModel guardarVehiculo(@RequestBody VehiculoModel vehiculo){
+        LocalDateTime fechaActual = LocalDateTime.now();
+        vehiculo.setCreated(fechaActual);
         return vehiculoService.guardarVehiculo(vehiculo);
     }
 
