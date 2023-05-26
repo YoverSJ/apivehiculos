be94f5acb85b38d7ffd46e543df6f83c7815de89
YoverSJ <ysantiago@geosatelital.com.pe>
2023-05-25 20:49:05 -0500

    implementacion de la clase Servicio

--- src/main/java/com/yoversj/apivehiculos/services/VehiculoService.java
+++ src/main/java/com/yoversj/apivehiculos/services/VehiculoService.java
@@ -1,5 +1,25 @@
 package com.yoversj.apivehiculos.services;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yoversj.apivehiculos.models.VehiculoModel;
+import com.yoversj.apivehiculos.repositories.VehiculoRepository;
+
+@Service
 public class VehiculoService {
+
+    @Autowired
+    private VehiculoRepository vehiculoRepository;
+
+    public ArrayList<VehiculoModel> obtenerVehiculos() {
+        return (ArrayList<VehiculoModel>)vehiculoRepository.findAll();
+    }
+
+    public VehiculoModel guardarVehiculo(VehiculoModel vehiculo) {
+        return vehiculoRepository.save(vehiculo);
+    }
     
 }
