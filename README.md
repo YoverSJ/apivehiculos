37226464008a85098bdea440168a398ff9fa2527
YoverSJ <ysantiago@geosatelital.com.pe>
2023-05-25 19:40:39 -0500

    proyecto inicial

---
+++ README.md
@@ -0,0 +1,4 @@
+# API Vehiculos
+
+### API de Vehiculos hecho con Spring Boot
+Con esta API podemos crear, listar, buscar, editar y eliminar un Vehiculo
---
+++ src/test/java/com/yoversj/apivehiculos/ApivehiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoversj.apivehiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApivehiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
